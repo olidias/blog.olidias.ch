dcafd5b0322ec85627eba2a1e74842172df88d95
Oli Dias <oliver.dias@zuehlke.com>
2022-11-09 16:43:58 +0100

    configure dotenv + use moment

--- src/pages/articleTeaser.js
+++ src/pages/articleTeaser.js
@@ -1,9 +1,12 @@
+import moment from "moment";
 import React from "react";
 
 export default function ArticleTeaser({article}) {
+    const publicationDate = moment(article.publicationDate);
     return article ? (
         <div>
             <h2>{article.title}</h2>
+            <p>{publicationDate.format('DD.MM.yyyy')}</p>
             <img src={article.coverImage.url} alt={article.coverImage.alt} height={100} width={100} />
             <button>Read Article</button>
         </div>
