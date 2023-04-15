324f58daa1dc160eafbd415d3174d151019a985f
Oli Dias <oli.dias94@proton.me>
2023-04-15 14:05:53 +0200

    fix console error

--- src/pages/article-teaser.js
+++ src/pages/article-teaser.js
@@ -5,8 +5,8 @@ export default function ArticleTeaser({ article }) {
     const publicationDate = moment(article?.publicationDate);
     return article ? (
         <a href={'articles/' + article.slug}
-            style={{ "background-image": `url(${article.coverImage?.url})` }}
-            className="w-full h-80 mt-10 shadow-xl px-10 py-5 bg-cover bg-no-repeat rounded flex flex-col items-stretch bg-right">
+            style={{'--image-url': `url('${article.coverImage?.url}')`}} 
+            className="bg-[image:var(--image-url)] w-full h-80 mt-10 shadow-xl px-10 py-5 bg-cover bg-no-repeat rounded flex flex-col items-stretch bg-right">
             <h2 className="text-lg font-semibold text-slate-800">{article.title}</h2>
             <p className="text-sm italic text-slate-700">{publicationDate.format('DD.MM.yyyy')}</p>
             <div className="align-middle my-auto text-slate-300 h-9 w-28 flex justify-center rounded border-2 border-slate-300 font-semibold backdrop-blur-xl mt-auto mb-0" >
